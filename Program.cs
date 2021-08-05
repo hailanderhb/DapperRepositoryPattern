f45da8aca9ca08efbd51adb3135b0f133af3ac5b
hailanderhb <hailanderhb@gmail.com>
2021-08-05 16:51:00 -0300

    Utilizando dapper, repository pattern, models, repositorio genérico

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost, 1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsers(connection);
+            ReadRoles(connection);
+            ReadTags(connection);
+            //UpdateUser(connection);
+            //DeleteUser(connection);
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            //DeleteUser();
+            connection.Close();
+
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        // --------------------------------------------------------------------------------------------
+
+        public static void UpdateUser(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var user = repository.Get(1);
+            user.Name = "Hailander Bastos";
+            repository.Update(user);
+
+        }
+
+        public static void DeleteUser(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var user = repository.Get(1);
+            repository.Delete(user);
+
+        }
+
+
+    }
+}
